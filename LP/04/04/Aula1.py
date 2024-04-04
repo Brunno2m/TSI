9c8aece7a6325056bd1393386174579d2d37e8be
brunno2m <brunno.m@escolar.ifrn.edu.br>
2024-04-04 08:22:08 -0300

    atualização de dados

--- LP/04/04/Aula1.py
+++ LP/04/04/Aula1.py
@@ -1,1 +1,9 @@
 #teste
+#estrutura if
+nota1 = int(input('Digite n1: '))
+nota2 = int(input('Digite n2: '))
+media = (nota1 + nota2)/2
+
+if (media >=70):
+    print(f'sua média é : {media} -->Aprovado')
+print('Fim do programa')    
